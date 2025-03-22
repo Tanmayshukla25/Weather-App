f4eb01234bc9ab6176acabdf747fcaf7225ff692
tanmay <tanmayshukla252@gmail.com>
2025-03-22 22:30:43 +0530

    second

--- weather.js
+++ weather.js
@@ -4,17 +4,21 @@ const btn = document.querySelector("#btn");
 const url = "https://api.openweathermap.org/data/2.5/weather?APPID=a3ff0ea5e54fa5a846957f72620b0699&units=metric"; 
 
 let date = new Date();
-let datetime=date.toDateString()
+let datetime = date.toDateString() + " || " + date.toLocaleTimeString();
 
-let elements = document.getElementById("now").innerHTML=datetime
+document.getElementById("now").innerHTML = datetime;
+
 
 
 
 btn.addEventListener("click", async (e) => {
     e.preventDefault();
-    
+   
     const forminput = input.value.trim();    
     fetchdata(forminput);
+    input.value = "";
+
+    
 });
 
 
@@ -28,8 +32,13 @@ async function fetchdata(place) {
         results.innerHTML = `
 
             <h2>${data.name}, ${data.sys.country}</h2>
-            <p>Temperature: ${data.main.temp} C</p>
+            <p>Temperature: ${data.main.temp_min} C</p>
+            <p>Temperature_max: ${data.main.temp_max} C</p>
+            <p>Temperature_min: ${data.main.temp} C</p>
             <p>Weather: ${data.weather[0].description}</p>
+            <p>wind-speed:${data.wind.speed}<P>
+            <p>wind-deg:${data.wind.deg}<P>
+            <p>clouds:${data.clouds.all}<P>
 
         `;
     } catch (error) {
